57517a2a8c2ee7e589d3504f5438cf3943ac162f
Mike D <michael.mcdaniel210@gmail.com>
2018-02-01 19:16:05 -0600

    working on movie-this portion

--- liri.js
+++ liri.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
-  var spotify = new Spotify(keys.spotify);
-  var client = new Twitter(keys.twitter);
-  var userInput = process.argv[2];
+// var request = require("request");
+// var twitter = require("twitter");
+// var spotify = new Spotify(keys.spotify);
+// var client = new Twitter(keys.twitter);
+var userCommand = process.argv[2];
+var userInput = process.argv[3];
+var request = require("request");
 
 
-
-switch (userInput){
+switch (userCommand){
 	case "my-tweets":
 	twatter();
 
@@ -31,7 +34,21 @@ function spotify (){
 	//get spotify API here
 }
 function movie (){
-	//get movie here
+	request("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+
+		if (!error && response.statusCode === 200) {
+			console.log(JSON.parse(body));
+			console.log("Title: " + JSON.parse(body).Title);
+			console.log("Year of Release: " + JSON.parse(body).Year);	    	
+			console.log("IMDb Rating: " + JSON.parse(body).imdbRating);
+			console.log("Rotten Tomatoes Rating: " + JSON.parse(body)['Ratings'][1]['Value']);
+			console.log();
+			console.log();
+			console.log();
+			console.log();
+			console.log();
+  		}
+});
 }
 function bkstBoys (){
 	//call up random.txt to get i want it that way displayed
