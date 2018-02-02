3feb0e3c4965525286a7137133e7b885f82592e5
Mike D <michael.mcdaniel210@gmail.com>
2018-02-01 22:04:10 -0600

    fixed issue when getting space before one word movies.  defined one word entries in userInput variable; built on one word userInput by using for loop to add onto the 3rd index process.argv

--- liri.js
+++ liri.js
@@ -3,13 +3,41 @@ var request = require("request");
 // var twitter = require("twitter");
 // var spotify = new Spotify(keys.spotify);
 // var client = new Twitter(keys.twitter);
+var multWords = process.argv;
 var userCommand = process.argv[2];
 var userInput = process.argv[3];
 
-	for (var i = 3; i < userInput.length; i++) {
-	userInput = userInput + " " + userInput[i];
-	}
+for (var i = 4; i < multWords.length; i++) {
+	userInput = userInput + " " + multWords[i];
 
+
+}
+	console.log(userInput);	
+function movie (){
+
+	request("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+
+		if (!error && response.statusCode === 200) {
+			console.log("");
+			console.log("Title: " + JSON.parse(body).Title);
+			console.log("");
+			console.log("Year of Release: " + JSON.parse(body).Year);
+			console.log("");	    	
+			console.log("IMDb Rating: " + JSON.parse(body).imdbRating);
+			console.log("");
+			console.log("Rotten Tomatoes Rating: " + JSON.parse(body)['Ratings'][1]['Value']);
+			console.log("");
+			console.log("Country: " + JSON.parse(body).Country);
+			console.log("");
+			console.log("Language: " + JSON.parse(body).Language);
+			console.log("");
+			console.log("Plot: " + JSON.parse(body).Plot);
+			console.log("");
+			console.log("Actors: " + JSON.parse(body).Actors);
+			console.log("");
+  		}
+});
+}
 
 switch (userCommand){
 	case "my-tweets":
@@ -37,31 +65,7 @@ function twatter () {
 function spotify (){
 	//get spotify API here
 }
-function movie (){
 
-	request("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
-
-		if (!error && response.statusCode === 200) {
-			console.log("");
-			console.log("Title: " + JSON.parse(body).Title);
-			console.log("");
-			console.log("Year of Release: " + JSON.parse(body).Year);
-			console.log("");	    	
-			console.log("IMDb Rating: " + JSON.parse(body).imdbRating);
-			console.log("");
-			console.log("Rotten Tomatoes Rating: " + JSON.parse(body)['Ratings'][1]['Value']);
-			console.log("");
-			console.log("Country: " + JSON.parse(body).Country);
-			console.log("");
-			console.log("Language: " + JSON.parse(body).Language);
-			console.log("");
-			console.log("Plot: " + JSON.parse(body).Plot);
-			console.log("");
-			console.log("Actors: " + JSON.parse(body).Actors);
-			console.log("");
-  		}
-});
-}
 function bkstBoys (){
 	//call up random.txt to get i want it that way displayed
 }
